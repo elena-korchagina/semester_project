3b8e23225c57168fd20f4f9c0c0e2ba22f18f820
elena-korchagina <64537000+elena-korchagina@users.noreply.github.com>
2020-06-01 00:45:44 +0200

    Update four.py

--- tasks/four.py
+++ tasks/four.py
@@ -1,31 +1,43 @@
 from .utils import random_up, remove_punctuation, dump_args
-
-# Fourth Task:
-
-
-"""This function """
-
-""" At first step we have to make our text free from punctuations , so that it doesn't mix up with the letters:"""
-
-""" Then we work on making some letters upper case and some lower case:"""
-""" And getting back the punctuation signs:"""
-
-
 @dump_args
+
+
 def fourth_task(s: str) -> str:
     """
-    randomly makes some letters upper case and some lower case
-    :param s: the string to modify....
+    Function randomly makes some letters of the words in a text upper case and some lower case
+    Parameters:
+    s(str): the string to modify
+    Returns:
+    str:string  with letters,randomly made upper case.
+    
+    Examples:
+        >>> fourth_task('What can we do with the drunken sailor?')
+        'What CAN wE do witH The DruNkeN SaILoR?'
+        >>> fourth_task('Because the sky is blue, it makes me cry')
+        'BECaUsE tHe SKY iS bLUe, IT MakeS ME Cry'
+      
+    Raises: TypeError in case of an empty string
+            ValueError in case that argument is not a string
+    
     """
+    
     if not isinstance(s, str):
         raise TypeError('s must be a non-empty string')
     elif s == '':
         raise ValueError('s must be a non-empty string')
 
+# At first step we have to make our text free from punctuations , so that it doesn't mix up with the letters:
+   
     List_of_Words = remove_punctuation(s).split()
+   
+# Then we work on making some letters upper case and some lower case:
+    
     List_of_Scrumbled_Words = []
     for elements in List_of_Words:
         List_of_Scrumbled_Words.append(random_up(elements))
+    
+# Finally we replace the words in the original text with the scrambled words, so that punctuation keeps on it's places:
+
     elements = len(List_of_Words)
     i = 0
     while i < elements:
