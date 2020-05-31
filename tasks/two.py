fd39831ca521cbdd2aafd3ad4ead1528cda350d3
elena-korchagina <64537000+elena-korchagina@users.noreply.github.com>
2020-05-31 22:52:47 +0200

    Update two.py

--- tasks/two.py
+++ tasks/two.py
@@ -1,33 +1,41 @@
 from .utils import shuffle_string, remove_punctuation, dump_args
 
-# Second Task:
-
-""" This function randomly reorders all the letters in a world."""
-
-""" At first step we have to make our text free from punctuations , so that it doesn't mix up with the letters:"""
-
-"""Then we again need a function that randomly reorders all the letters in a word: """
-
-""" After that we can apply it to a sentence:"""
-
-""" And finally get back the punctuation by changing all the words in the original text:"""
-
-
 @dump_args
+
 def second_task(s : str) -> str:
+    """ This function randomly reorders all the letters in a world.
+    
+    Args: takes a string (text)
+    
+    Returns: string (text) with randomly reordered letters of each word in a text.
+    
+    Examples: 
+    What can we do with the drunken sailor? -> Tahw acn ew od hwit the unekrnd lsaoir?
+    
+    Because the sky is blue, it makes me cry -> eaucBes teh ysk is elbu, ti kemas em cyr
+    
+    Raises: TypeError in case of an empty string
+            ValueError in case that argument is not a string
+    
+    """
     if not isinstance(s, str):
         raise TypeError('s must be a non-empty string')
     elif s == '':
         raise ValueError('s must be a non-empty string')
+        
+# At first step we have to make our text free from punctuations , so that it doesn't mix up with the letters:
 
     List_of_Words = remove_punctuation(s).split()
+#Then we  need a function shuffle_string that randomly reorders all the letters in a word, we can find it in utils: """
     List_of_Scrumbled_Words = []
     for elements in List_of_Words:
         shuffle_element=shuffle_string(elements)
+# Only for words, that are longer than one letter, we need to make shure, that letters were reordered properly        
         if len(elements) > 1:            
             while shuffle_element == elements:
                 shuffle_element=shuffle_string(elements)
         List_of_Scrumbled_Words.append(shuffle_element)
+# And finally get back the punctuation by changing all the words in the original text with the new, reorganised words:"""    
     elements = len(List_of_Words)
     i = 0
     while i < elements:
